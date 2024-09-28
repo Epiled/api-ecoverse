0f2933e83590d7970ae804a8fc3236d623942f94
Felipe De Andrade <felipe.deam98@gmail.com>
2024-09-28 04:46:49 -0300

    fix: API

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const path = require('path');
 
 const app = express();
 
-app.use(cors()); // Adiciona suporte a CORS
+app.use(cors());
 app.use(express.json());
 
 // Endpoint para servir os produtos do arquivo JSON
-app.get('/api/products', (req, res) => {
-  const products = require(path.join(__dirname, 'db/products.json'));
-  res.json(products);
+app.get('/api/produtos', (req, res) => {
+  try {
+    const products = require(path.join(__dirname, 'db/produtos.json'));
+    res.json(products);
+  } catch (error) {
+    console.error(error); // Log do erro
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 });
 
 // Definindo a porta
