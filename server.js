45e0a1cf004718684c8d1f044bed447e3fa06cda
Felipe De Andrade <felipe.deam98@gmail.com>
2024-09-28 15:45:42 -0300

    fix: Route URL

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 // Endpoint para servir os produtos do arquivo JSON
-app.get('/api/produtos', (req, res) => {
+app.get('/api/products', (req, res) => {
   try {
     const products = require(path.join(__dirname, 'db/produtos.json'));
     res.json(products);
