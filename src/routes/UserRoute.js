08dac5b7f7183cd2a823e6b6121cb424f12145ce
Felipe De Andrade <felipe.deam98@gmail.com>
2026-05-10 10:50:42 -0300

    refactor(routes): optimize routing structure and implement RBAC across endpoints

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -1,17 +1,24 @@
 import express from "express";
-
 import UserController from "../controllers/UserController.js";
-
 import authenticated from "../middleware/authenticated.js";
+import authorize from "../middleware/authorize.js";
+import isOwnerOrAdmin from "../middleware/owner.js";
+
+import { ROLES } from "../constants/roles.js";
+
+const { ADMIN } = ROLES;
 
 const router = express.Router();
 
+router.post("/", UserController.postUser);
+
 router.use(authenticated);
 
-router.get("/api/users", UserController.getUsers);
-router.get("/api/users/:id", UserController.getUserById);
-router.post("/api/users", UserController.postUser);
-router.patch("/api/users/:id", UserController.patchUser);
-router.delete("/api/users/:id", UserController.deleteUser);
+router.get("/", authorize([ADMIN]), UserController.getUsers);
+
+router.get("/:id", isOwnerOrAdmin, UserController.getUserById);
+router.patch("/:id", isOwnerOrAdmin, UserController.patchUser);
+
+router.delete("/:id", authorize([ADMIN]), UserController.deleteUser);
 
 export default router;
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,12 +1,24 @@
 import express from "express";
-
 import ProductsController from "../controllers/ProductsController.js";
+import authenticated from "../middleware/authenticated.js";
+import authorize from "../middleware/authorize.js";
+
+import { ROLES } from "../constants/roles.js";
+
+const { ADMIN, MANAGER } = ROLES;
 
 const router = express.Router();
 
-router.get("/api/products", ProductsController.getProducts);
-router.post("/api/products", ProductsController.postProduct);
-router.patch("/api/products/:id", ProductsController.patchProduct);
-router.delete("/api/products/:id", ProductsController.deleteProduct);
+router.get("/", ProductsController.getProducts);
+
+router.use(authenticated);
+
+router.post("/", authorize([ADMIN, MANAGER]), ProductsController.postProduct);
+router.patch(
+  "/:id",
+  authorize([ADMIN, MANAGER]),
+  ProductsController.patchProduct,
+);
+router.delete("/:id", authorize([ADMIN]), ProductsController.deleteProduct);
 
 export default router;
