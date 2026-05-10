ed587e2ea1e7ae4d7f08cf86024530cc033c36b1
Felipe De Andrade <felipe.deam98@gmail.com>
2026-05-10 10:49:11 -0300

    feat(security): apply RBAC guards to all sensitive endpoints

--- src/routes/CategoryRoute.js
+++ src/routes/CategoryRoute.js
@@ -1,15 +1,26 @@
 import express from "express";
 import CategoryController from "../controllers/CategoryController.js";
 import authenticated from "../middleware/authenticated.js";
+import authorize from "../middleware/authorize.js";
+
+import { ROLES } from "../constants/roles.js";
+
+const { ADMIN, MANAGER } = ROLES;
 
 const router = express.Router();
 
 router.get("/", CategoryController.getCategories);
 router.get("/:id", CategoryController.getCategoryById);
-router.post("/", CategoryController.postCategory);
-router.patch("/:id", CategoryController.patchCategory);
-router.delete("/:id", CategoryController.deleteCategory);
 
 router.use(authenticated);
+
+router.post("/", authorize([ADMIN, MANAGER]), CategoryController.postCategory);
+router.patch(
+  "/:id",
+  authorize([ADMIN, MANAGER]),
+  CategoryController.patchCategory,
+);
+
+router.delete("/:id", authorize([ADMIN]), CategoryController.deleteCategory);
 
 export default router;
--- src/routes/SubcategoryRoute.js
+++ src/routes/SubcategoryRoute.js
@@ -1,12 +1,33 @@
 import express from "express";
 import SubcategoryController from "../controllers/SubcategoryController.js";
+import authenticated from "../middleware/authenticated.js";
+import authorize from "../middleware/authorize.js";
+
+import { ROLES } from "../constants/roles.js";
+
+const { ADMIN, MANAGER } = ROLES;
 
 const router = express.Router();
 
 router.get("/", SubcategoryController.getSubcategories);
 router.get("/:id", SubcategoryController.getSubcategoryById);
-router.post("/", SubcategoryController.postSubcategory);
-router.patch("/:id", SubcategoryController.patchSubcategory);
-router.delete("/:id", SubcategoryController.deleteSubcategory);
+
+router.use(authenticated);
+
+router.post(
+  "/",
+  authorize([ADMIN, MANAGER]),
+  SubcategoryController.postSubcategory,
+);
+router.patch(
+  "/:id",
+  authorize([ADMIN, MANAGER]),
+  SubcategoryController.patchSubcategory,
+);
+router.delete(
+  "/:id",
+  authorize([ADMIN]),
+  SubcategoryController.deleteSubcategory,
+);
 
 export default router;
